faec1d29e15af8d9c23b09d9e2d438572fd1ad3b
tienpvse <good66612@gmail.com>
2024-04-16 12:33:25 +0700

    resolve: eager loading neogit plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,9 +8,12 @@ return {
   },
   {
     "NeogitOrg/neogit",
+    lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "sindrets/diffview.nvim",
+
+      "nvim-telescope/telescope.nvim",
     },
     config = true,
   },
