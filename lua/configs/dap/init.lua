5ec4b412f450df1376349f187d02d9b28e90e9e4
tienpvse <good66612@gmail.com>
2024-06-24 21:19:50 +0700

    add-things

--- lua/configs/dap/init.lua
+++ lua/configs/dap/init.lua
@@ -1,18 +1,20 @@
 local M = {}
-M.config = function()
-  local dapui = require "dapui"
-  dapui.setup()
-  require "configs.dap.go"
-  require "configs.dap.node"
-  require "configs.dap.ui"
-  require "configs.dap.mappings"
-end
+M = {
+  config = function()
+    local dapui = require "dapui"
+    dapui.setup()
+    require "configs.dap.go"
+    require "configs.dap.node"
+    require "configs.dap.ui"
+    require "configs.dap.mappings"
+  end,
 
-M.dependencies = {
-  "rcarriga/nvim-dap-ui",
-  "leoluz/nvim-dap-go",
-  "nvim-neotest/nvim-nio",
-  "mxsdev/nvim-dap-vscode-js",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "leoluz/nvim-dap-go",
+    "nvim-neotest/nvim-nio",
+    "mxsdev/nvim-dap-vscode-js",
+  },
 }
 
 return M
